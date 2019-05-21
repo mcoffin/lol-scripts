4325da62069fc71e19ba7a47a2b18488142e42ff
Matt Coffin <mcoffin13@gmail.com>
2019-05-21 14:22:07 -0600

    league-client: Wait a little longer (concurrency issue with threads not being spawned yet)

--- league-client.sh
+++ league-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 nonclient_processes=(
 	'wineserver'
 	'LeagueClientUx.exe'
@@ -19,6 +19,6 @@ pushd /sys/fs/cgroup/cpuset
 for pname in "${nonclient_processes[@]}"; do
 	wait_until_pid "$pname"
 done
-sleep 3;
+sleep 5;
 popd
 exec bash client-cgroups.sh
