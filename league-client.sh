d230608785f6da92c22673b7a73cb0bf23b5a330
Matt Coffin <mcoffin13@gmail.com>
2019-06-08 12:22:30 -0600

    league-client: Make script runnable from outside the League of Legends directory

--- league-client.sh
+++ league-client.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -x
+if [ -z "$LOL_DIR" ]; then
+	LOL_DIR="$WINEPREFIX/drive_c/Riot Games/League of Legends"
+fi
+
+if [ ! -d /sys/fs/cgroup/cpuset/league_client ]; then
+	(>&2 echo "league_client cgroup not created.\nConsider running setup.sh first")
+	exit 1
+fi
+
 nonclient_processes=(
 	'wineserver'
 	'LeagueClientUx.exe'
@@ -14,11 +23,22 @@ wait_until_pid() {
 	done
 }
 
-WINEDEBUG=-all DXVK_FILTER_DEVICE_NAME=590 cgexec -g cpuset:league_client wine LeagueClient.exe
-pushd /sys/fs/cgroup/cpuset
+# If the client isn't already launched, launch it.
+current_pid="$(pidof LeagueClient.exe)"
+if [ -z "$current_pid" ]; then
+	pushd "$LOL_DIR"
+	WINEDEBUG=-all DXVK_FILTER_DEVICE_NAME=590 cgexec -g cpuset:league_client wine LeagueClient.exe
+	for pname in "${nonclient_processes[@]}"; do
+		wait_until_pid "$pname"
+	done
+	sleep 5;
+	popd
+fi
+
+# Now, set the processes to the correct cgroups
+cd /sys/fs/cgroup/cpuset
 for pname in "${nonclient_processes[@]}"; do
-	wait_until_pid "$pname"
+	pid=$(pidof "$pname")
+	echo "$pname:$pid"
+	pidof -S '\n' "$pname" >> cgroup.procs
 done
-sleep 5;
-popd
-exec bash client-cgroups.sh
